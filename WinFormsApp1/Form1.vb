7d6d27d9df68e640a88f25d802f439498f7a7227
UJ4NG <86789381+udinJmng@users.noreply.github.com>
2026-09-08 08:48:47 +0700

    refactor(fix) : back to normal

--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -3,7 +3,7 @@
 
     Private Sub BtnClick_Click(sender As Object, e As EventArgs) Handles BtnClick.Click
 
-        res = CInt(tb1.Text + tb2.Text) 'bisa cint + convert int32'
+        res = CInt(tb1.Text) + CInt(tb2.Text) 'bisa cint + convert int32'
         getRes.Text = res
 
     End Sub
