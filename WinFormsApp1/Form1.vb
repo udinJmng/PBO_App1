c1e90a409e5a2734c483c29146b897ca43821b71
UJ4NG <86789381+udinJmng@users.noreply.github.com>
2026-09-08 08:07:14 +0700

    refactor(clean) : clean useless code

--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -1,12 +1,3 @@
 ﻿Public Class Form1
-    'Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
-    'End Sub
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-        judulCOK.Text = "hello world"
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles judulCOK.Click
-
-    End Sub
 End Class
