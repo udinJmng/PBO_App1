7afebc64cf3853faa429f5fef6c29c29c64fb7b6
UJ4NG <86789381+udinJmng@users.noreply.github.com>
2026-09-08 08:54:16 +0700

    feate(validation) : can't input min number (example : -2 or anything else)

--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -3,6 +3,11 @@
 
     Private Sub BtnClick_Click(sender As Object, e As EventArgs) Handles BtnClick.Click
 
+        If CInt(tb1.Text) < 0 OrElse CInt(tb2.Text) < 0 Then
+            getRes.Text = "You can't operate"
+            Return
+        End If
+
         res = CInt(tb1.Text) + CInt(tb2.Text) 'bisa cint + convert int32'
         getRes.Text = res
 
