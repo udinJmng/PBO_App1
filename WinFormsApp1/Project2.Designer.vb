cd00f9a7a3ca902b9143ecbb607ee1172eeddae3
UJ4NG <86789381+udinJmng@users.noreply.github.com>
2026-09-08 09:25:38 +0700

    feat(project 2) : learn button, checkboxt, auto input

--- WinFormsApp1/Project2.Designer.vb
+++ WinFormsApp1/Project2.Designer.vb
@@ -27,29 +27,31 @@ Partial Class Project2
         CheckBox2 = New CheckBox()
         TextBox1 = New TextBox()
         TextBox2 = New TextBox()
-        RadioButton1 = New RadioButton()
-        RadioButton2 = New RadioButton()
+        sButton = New RadioButton()
         TextBox3 = New TextBox()
         Button1 = New Button()
+        bButton = New RadioButton()
+        Button2 = New Button()
         SuspendLayout()
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
+        Label1.ForeColor = Color.Coral
         Label1.Location = New Point(156, 57)
         Label1.Name = "Label1"
-        Label1.Size = New Size(41, 15)
+        Label1.Size = New Size(85, 15)
         Label1.TabIndex = 0
-        Label1.Text = "Label1"
+        Label1.Text = "Mamat koding"
         ' 
         ' CheckBox1
         ' 
         CheckBox1.AutoSize = True
         CheckBox1.Location = New Point(156, 99)
         CheckBox1.Name = "CheckBox1"
-        CheckBox1.Size = New Size(84, 19)
+        CheckBox1.Size = New Size(51, 19)
         CheckBox1.TabIndex = 1
-        CheckBox1.Text = "CheckBox1"
+        CheckBox1.Text = "OOP"
         CheckBox1.UseVisualStyleBackColor = True
         ' 
         ' CheckBox2
@@ -57,9 +59,9 @@ Partial Class Project2
         CheckBox2.AutoSize = True
         CheckBox2.Location = New Point(156, 128)
         CheckBox2.Name = "CheckBox2"
-        CheckBox2.Size = New Size(84, 19)
+        CheckBox2.Size = New Size(40, 19)
         CheckBox2.TabIndex = 2
-        CheckBox2.Text = "CheckBox2"
+        CheckBox2.Text = "VB"
         CheckBox2.UseVisualStyleBackColor = True
         ' 
         ' TextBox1
@@ -76,27 +78,16 @@ Partial Class Project2
         TextBox2.Size = New Size(235, 23)
         TextBox2.TabIndex = 4
         ' 
-        ' RadioButton1
+        ' sButton
         ' 
-        RadioButton1.AutoSize = True
-        RadioButton1.Location = New Point(177, 284)
-        RadioButton1.Name = "RadioButton1"
-        RadioButton1.Size = New Size(97, 19)
-        RadioButton1.TabIndex = 5
-        RadioButton1.TabStop = True
-        RadioButton1.Text = "RadioButton1"
-        RadioButton1.UseVisualStyleBackColor = True
-        ' 
-        ' RadioButton2
-        ' 
-        RadioButton2.AutoSize = True
-        RadioButton2.Location = New Point(460, 284)
-        RadioButton2.Name = "RadioButton2"
-        RadioButton2.Size = New Size(97, 19)
-        RadioButton2.TabIndex = 6
-        RadioButton2.TabStop = True
-        RadioButton2.Text = "RadioButton2"
-        RadioButton2.UseVisualStyleBackColor = True
+        sButton.AutoSize = True
+        sButton.Location = New Point(460, 284)
+        sButton.Name = "sButton"
+        sButton.Size = New Size(53, 19)
+        sButton.TabIndex = 6
+        sButton.TabStop = True
+        sButton.Text = "Salah"
+        sButton.UseVisualStyleBackColor = True
         ' 
         ' TextBox3
         ' 
@@ -114,15 +105,36 @@ Partial Class Project2
         Button1.Text = "Button1"
         Button1.UseVisualStyleBackColor = True
         ' 
+        ' bButton
+        ' 
+        bButton.AutoSize = True
+        bButton.Location = New Point(177, 284)
+        bButton.Name = "bButton"
+        bButton.Size = New Size(55, 19)
+        bButton.TabIndex = 9
+        bButton.TabStop = True
+        bButton.Text = "Benar"
+        bButton.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(482, 376)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 10
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
         ' Project2
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(672, 450)
+        Controls.Add(Button2)
+        Controls.Add(bButton)
         Controls.Add(Button1)
         Controls.Add(TextBox3)
-        Controls.Add(RadioButton2)
-        Controls.Add(RadioButton1)
+        Controls.Add(sButton)
         Controls.Add(TextBox2)
         Controls.Add(TextBox1)
         Controls.Add(CheckBox2)
@@ -140,7 +152,9 @@ Partial Class Project2
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents RadioButton1 As RadioButton
-    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents sButton As RadioButton
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents bButton As RadioButton
+    Friend WithEvents Button2 As Button
 End Class
--- WinFormsApp1/Project2.vb
+++ WinFormsApp1/Project2.vb
@@ -1,3 +1,38 @@
 ﻿Public Class Project2
 
+    Sub ClearAll()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+
+        CheckBox1.Checked = False
+        CheckBox2.Checked = False
+        bButton.Checked = False
+        sButton.Checked = False
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = False Then
+            TextBox1.Text = ""
+            Return
+        End If
+        TextBox1.Text = "Object Oriented Programming"
+    End Sub
+
+    Private Sub vb_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = False Then
+            TextBox2.Text = ""
+            Return
+        End If
+        TextBox2.Text = "Visual Basic"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ClearAll()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DestroyHandle()
+    End Sub
 End Class
