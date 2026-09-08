ec7d7be24c1c592bd86a1b2a2b3344a7d8dda198
UJ4NG <86789381+udinJmng@users.noreply.github.com>
2026-09-08 08:36:59 +0700

    refactor(clean) : remove useless variable at convertion

--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -2,8 +2,6 @@
     Dim res As Integer
 
     Private Sub BtnClick_Click(sender As Object, e As EventArgs) Handles BtnClick.Click
-        Dim bil1 As Integer
-        Dim bil2 As Integer
 
         res = CInt(tb1.Text) + CInt(tb2.Text) 'bisa cint + convert int32'
         getRes.Text = res
